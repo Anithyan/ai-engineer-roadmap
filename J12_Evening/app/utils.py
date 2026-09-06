f6a14dba27d3d1178e4badeba894eceda46dbcda
Ani <pc_ubuntu_ani@DESKTOP-QM538E1.localdomain>
2026-09-06 20:35:35 +0200

    fix: restore  apply_discount

--- J12_Evening/app/utils.py
+++ J12_Evening/app/utils.py
@@ -1,9 +1,9 @@
 # app/utils.py
 
 
-# def apply_discount(price: float, pct: float) -> float:
-#     return price * (1 - pct / 100)
-
-
 def apply_discount(price: float, pct: float) -> float:
-    return price * (1 - pct / 100) + 1  # ← bug volontaire (+1)
+    return price * (1 - pct / 100)
+
+
+# def apply_discount(price: float, pct: float) -> float:
+#     return price * (1 - pct / 100) + 1  # ← bug volontaire (+1)
